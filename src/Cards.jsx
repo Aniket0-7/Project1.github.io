0fa8dc2d98d491477439d33c90459f7297d78d71
Aniket Singh <aniker207@gmail.com>
2023-03-14 18:10:29 +0530

    css/change

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,9 +3,12 @@ import React from "react";
 
 function Card(props) {
 
-    return (<div className="cards">
+    return (
         <div className="card">
-            <img src={props.img_src} alt="img" className="card_img" />
+        <div className="imgcon">
+        <img src={props.img_src} alt="img" className="card_img" />
+        </div>
+            
             <div className="card_info">
                 <span className="card_category"> {props.title} </span>
                 <h3 className="card_title"> {props.sname} </h3>
@@ -14,7 +17,7 @@ function Card(props) {
                 </a>
             </div>
         </div>
-    </div>)
+    )
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import  Card  from "./Cards";
+import Card from "./Cards";
 import "./index.css";
-import  Sdata  from "./Sdata";
+import Sdata from "./Sdata";
 
 const ncard = (val) => {
-  return(
-    <Card 
-    img_src={val.img_src}
-            title={val.title}
-            sname={val.sname}
-            link={val.link}
-            />
+  return (
+    
+      <Card
+        img_src={val.img_src}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+      />
+    
   )
 }
 
 ReactDOM.render(
-    <>  <h1 className="heading">Top netflix series</h1>,
+  <>  <h1 className="heading">Top netflix series</h1>
+  <div className="cards">
     {Sdata.map(ncard)}
-        
-    </>,
-    document.getElementById('root'));
+    <div className="name">&copy; Made by Gogeta(Aniket)</div>
+    </div>
+
+  </>,
+  document.getElementById('root'));
